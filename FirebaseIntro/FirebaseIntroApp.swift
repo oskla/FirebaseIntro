2205fbefa81e50984842373e182f187bb281b4c9
oskla <oskarlarssson@gmail.com>
2022-10-12 09:29:13 +0200

    Initial Commit

---
+++ FirebaseIntro/FirebaseIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseIntroApp.swift
+//  FirebaseIntro
+//
+//  Created by Oskar Larsson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
